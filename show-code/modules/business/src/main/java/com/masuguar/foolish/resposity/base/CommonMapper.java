0cd6e07de80cec5d7cf03ab163d80f57d6bda23b
masuguar <my_mymsn@hotmail.com>
2018-12-11 06:29:38 +0800

    随便改改

--- show-code/modules/business/src/main/java/com/masuguar/foolish/resposity/base/CommonMapper.java
+++ show-code/modules/business/src/main/java/com/masuguar/foolish/resposity/base/CommonMapper.java
@@ -4,10 +4,13 @@ import tk.mybatis.mapper.common.ConditionMapper;
 import tk.mybatis.mapper.common.base.BaseDeleteMapper;
 import tk.mybatis.mapper.common.base.BaseInsertMapper;
 import tk.mybatis.mapper.common.base.BaseUpdateMapper;
+import tk.mybatis.mapper.common.base.insert.InsertMapper;
 import tk.mybatis.mapper.common.base.select.SelectAllMapper;
 import tk.mybatis.mapper.common.base.select.SelectByPrimaryKeyMapper;
+import tk.mybatis.mapper.common.special.InsertListMapper;
 
-public interface CommonMapper<T> extends BaseInsertMapper<T>,
+public interface CommonMapper<T> extends InsertMapper<T>,
+        InsertListMapper<T>,
         SelectAllMapper<T>,
         SelectByPrimaryKeyMapper<T>,
         BaseDeleteMapper<T>,
