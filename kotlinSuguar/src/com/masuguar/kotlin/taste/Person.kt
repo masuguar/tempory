9fd409d1da9130261bfb4410432c03cafa2a5ac1
masuguar <my_mymsn@hotmail.com>
2019-01-05 19:59:04 +0800

    删除没有必要的模块

---
+++ kotlinSuguar/src/com/masuguar/kotlin/taste/Person.kt
@@ -0,0 +1,2 @@
+package com.masuguar.kotlin.taste
+
--- show-code/clean-split-table/src/main/java/com/masuguar/foolish/format/Formatter.java
+++
@@ -1,8 +0,0 @@
-package com.masuguar.foolish.format;
-
-public interface Formatter<T> {
-
-    public String preffix( T t );
-
-    public String suffix( T t );
-}
